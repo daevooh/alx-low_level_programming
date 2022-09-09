0d50e919d5b210ea558b0cbaaf5e995704be21ad
daevooh <ogbekingdavid@outlook.com>
2022-09-09 15:27:39 -0700

    this files might cause a conflit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main (void)
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+  
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);  
